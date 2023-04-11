382607b7dabf36fb086baadc82ec0cd1948a6144
sivaShilpa <shilpa.vchand@gmail.com>
2023-04-11 18:48:38 -0400

    complete finch collector part1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+finches = [
+    {'name': 'Mynah', 'breed': 'European goldfinch', 'age': 2},
+    {'name': 'April', 'breed': 'House finch', 'age': 3},
+    {'name': 'Daisy', 'breed': 'Common chaffinch', 'age': 1},
+    {'name': 'Blossom', 'breed': 'American goldfinch', 'age': 4},
+    {'name': 'Cookie', 'breed': 'Eurasian bullfinch', 'age': 5}    
+]
+
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {'finches': finches})
