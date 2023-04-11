d93d1f28a0f641f1fd97abf1edfe5453836e3de4
sivaShilpa <shilpa.vchand@gmail.com>
2023-04-11 17:54:00 -0400

    secret key issue resolved

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4j01vd0^3-37y0_@^a8obmyw8xxdaq!l2w#v@8%sr%92=_5em^'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
