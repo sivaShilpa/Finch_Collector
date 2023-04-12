aa5384859f2b2b95371610842392e4a64c3e0629
sivaShilpa <shilpa.vchand@gmail.com>
2023-04-12 14:08:47 -0400

    Complete Finch_Collector Lab part2

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-
+from .models import Finch
 # Create your views here.
-finches = [
-    {'name': 'Mynah', 'breed': 'European goldfinch', 'age': 2},
-    {'name': 'April', 'breed': 'House finch', 'age': 3},
-    {'name': 'Daisy', 'breed': 'Common chaffinch', 'age': 1},
-    {'name': 'Blossom', 'breed': 'American goldfinch', 'age': 4},
-    {'name': 'Cookie', 'breed': 'Eurasian bullfinch', 'age': 5}    
-]
+# finches = [
+#     {'name': 'Mynah', 'breed': 'European goldfinch', 'age': 2},
+#     {'name': 'April', 'breed': 'House finch', 'age': 3},
+#     {'name': 'Daisy', 'breed': 'Common chaffinch', 'age': 1},
+#     {'name': 'Blossom', 'breed': 'American goldfinch', 'age': 4},
+#     {'name': 'Cookie', 'breed': 'Eurasian bullfinch', 'age': 5}    
+# ]
 
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
@@ -17,4 +17,9 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', {'finches': finches})
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch})
